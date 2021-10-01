f880d4020a31a2a7b7a4487a5e9e67bd98e7a42d
Joel Colaco <jcolaco@brocksolutions.com>
2021-10-01 15:30:47 -0400

    - Fixing up script and adding grid to filename

--- add_grid.py
+++ add_grid.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 from gimpfu import *
-def run(sourceFolder, outputFolder, gridColor):
+def run(sourceFolder, outputFolder, gridColor, dpi, lineThickness):
 
     if not os.path.exists(outputFolder):
         os.makedirs(outputFolder)
@@ -9,7 +9,8 @@ def run(sourceFolder, outputFolder, gridColor):
     filenames = [f for f in os.listdir(sourceFolder) if os.path.isfile(os.path.join(sourceFolder, f))]
     for filename in filenames:
         sourceFile = os.path.join(sourceFolder, filename)
-        outputFile = os.path.join(outputFolder, filename)
+        outputFileName = "Grid_" + filename
+        outputFile = os.path.join(outputFolder, outputFileName)
         image = loadImage(sourceFile)
         addGrid(image, gridColor)
         saveImage(outputFile, image)	
@@ -48,18 +49,20 @@ def add_grid_script(customtext, font, size):
   gimp.Display(img)
   gimp.displays_flush()
 register(
- "python_test2",
- "TEST2",
- "TEST2",
- "Brad Jones",
- "Brad Jones",
- "2017",
- "TEST2",
+ "python_fu_add_grid",
+ "Process files in the specified folder and add a grid to them  ",
+ "Process files in the specified folder and add a grid to them  ",
+ "Joel Colaco",
+ "Joel Colaco",
+ "2021",
+ "Generate Grids",
  "",
  [
  (PF_DIRNAME, "sourceFolder", "Source directory", "C:\Users\jcolaco\Pictures\Python Test\Source"),
  (PF_DIRNAME, "outputFolder", "Output directory", "C:\Users\jcolaco\Pictures\Python Test\Out"),
- (PF_COLOR, "gridColor", "select a color:", (0, 128, 255) )
+ (PF_COLOR, "gridColor", "select a color:", (0, 0, 0) ),
+ (PF_INT32, "dpi", "DPI", 70),
+ (PF_INT32, "lineThickness", "Grid Thickness", 1) 
  ],
  [],
  run, menu="<Image>/File/Create")
